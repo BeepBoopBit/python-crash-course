3da85eeb459a4cbe665994676237f59314b9d448
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 07:50:18 +0800

    Added 2.4

---
+++ Variables And Simple Data Types/2.4.py
@@ -0,0 +1,7 @@
+first_name = "water"
+message = f"Hello {first_name.upper()}, do you like watermelon?"
+print(message)
+message = f"Hello {first_name.lower()}, do you like watermelon?"
+print(message)
+message = f"Hello {first_name.title()}, do you like watermelon?"
+print(message)
