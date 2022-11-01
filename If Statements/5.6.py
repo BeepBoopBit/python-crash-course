ca011cec6ef4c472c2c2f1c5d82bf672e753e774
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 12:30:01 +0800

    Added 5.6.py

---
+++ If Statements/5.6.py
@@ -0,0 +1,14 @@
+player_age = 18
+
+if player_age < 2:
+    print("You are a baby.")
+elif player_age < 4:
+    print("You are a toddler.")
+elif player_age < 13:
+    print("You are a kid.")
+elif player_age < 20:
+    print("You are a teenager.")
+elif player_age < 65:
+    print("you are an adult.")
+else:
+    print("You are and elder")
