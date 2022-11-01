2921090ef70918699d584d7e7afc2c590ccb483f
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 16:07:40 +0800

    Added 7.2.py

---
+++ User Input and While Loops/7.2.py
@@ -0,0 +1,8 @@
+user_input = input("Enter how many number of people are dining: ")
+
+if user_input > 8:
+    print("You have to wait for a table")
+else:
+    print("Welcome to the restaurant")
+    
+    
