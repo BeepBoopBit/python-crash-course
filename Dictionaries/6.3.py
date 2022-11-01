f15c82c18bd4f9a0831ab43226509fad49ebe840
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 13:24:26 +0800

    Added Dictionaries

---
+++ Dictionaries/6.3.py
@@ -0,0 +1,15 @@
+glossary = {
+    'dictionary' : 'A collection of key-value pairs.',
+    'list' : 'A collection of items that is mutable',
+    'tuple' : 'A collection of immutable items',
+    'variables' : 'Label for values',
+    'mutable' : 'something that can be changed',
+    'immutable' : 'something that cannot be changed',
+}
+
+print(f"Dictionary: {glossary['dictionary']}")
+print(f"List: {glossary['list']}")
+print(f"Tuple: {glossary['tuple']}")
+print(f"Variables: {glossary['variables']}")
+print(f"Mutable: {glossary['mutable']}")
+print(f"Immutable: {glossary['immutable']}")
