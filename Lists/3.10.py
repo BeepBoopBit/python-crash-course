b730c675ebbe854e814110be181d7d0262ae1ecc
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 09:37:24 +0800

    Added 3.10.py

---
+++ Lists/3.10.py
@@ -0,0 +1,29 @@
+things = ['water', 'melon', 'papaya', 'banana', 'fruit', 'salad']
+
+print(sorted(things))
+
+print(things)
+
+things.sort()
+
+print(things)
+
+things.reverse()
+
+print(things)
+
+things.sort(reverse=True)
+
+print (things)
+
+print(len(things))
+
+print(things[0])
+print(things.pop())
+print(things.pop(1))
+
+del things[2]
+
+things.append("stuff")
+things.insert(2,"eme")
+print(sorted(things))
