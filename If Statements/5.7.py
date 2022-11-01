16d6d076f00308c1ecb0b555bb0a11ec1a848ee5
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 12:31:18 +0800

    Added 5.7.py

---
+++ If Statements/5.7.py
@@ -0,0 +1,10 @@
+fruits = ['water','melon','papaya','banana']
+
+if 'water' in fruits:
+    print("YEHEY")
+if 'melon' in fruits:
+    print("YEHEY")
+if 'papaya' in fruits:
+    print("YEHEY")
+if 'banana' in fruits:
+    print("YEHEY")
