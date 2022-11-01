58d22a1791794192789f38a3dd921f83a92ee127
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 09:02:12 +0800

    Added 3.6.py

--- Lists/3.6.py
+++ Lists/3.6.py
@@ -30,3 +30,21 @@ print(f"Hello {guest_list[2]}, {message}")
 print(f"Hello {guest_list[3]}, {message}")
 print(f"Hello {guest_list[4]}, {message}")
 print(f"Hello {guest_list[5]}, {message}")
+
+
+print("I'm sorry guys, but I can't invite more people since the table wouldn't arrive in time")
+
+print(f"I'm sorry {guest_list.pop()}, you can't come to the dinner party anymore")
+print(f"I'm sorry {guest_list.pop()}, you can't come to the dinner party anymore")
+print(f"I'm sorry {guest_list.pop()}, you can't come to the dinner party anymore")
+print(f"I'm sorry {guest_list.pop()}, you can't come to the dinner party anymore")
+
+
+message = "You are still invited to the dinner party"
+print(f"Hello {guest_list[0]}, {message}")
+print(f"Hello {guest_list[1]}, {message}")
+
+del guest_list[0]
+del guest_list[1]
+
+print(guest_list)
