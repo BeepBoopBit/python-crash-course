1f26383777087cde6d1576ab4f59925d2a46b2e2
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 08:56:31 +0800

    Added 3.4.py

---
+++ Lists/3.4.py
@@ -0,0 +1,7 @@
+guest_list = ["water", "melon", "papaya"]
+
+message = "I would like to invite you to a dinner party"
+
+print(f"Hello {guest_list[0]}, {message}")
+print(f"Hello {guest_list[1]}, {message}")
+print(f"Hello {guest_list[2]}, {message}")
