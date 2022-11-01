ee34e78e7bca6f33c8e4097828b3467be115dead
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 13:20:40 +0800

    Added 6.2.py

---
+++ Dictionaries/6.2.py
@@ -0,0 +1,15 @@
+favorite_number = {
+    'water':1,
+    'melon':2,
+    'papaya':3,
+    'banana':4,
+    'fruit':5,
+    'salad':6,
+}
+
+print(f"Water's favorite number is {favorite_number['water']}")
+print(f"melon's favorite number is {favorite_number['melon']}")
+print(f"papaya's favorite number is {favorite_number['papaya']}")
+print(f"banana's favorite number is {favorite_number['banana']}")
+print(f"fruit's favorite number is {favorite_number['fruit']}")
+print(f"salad's favorite number is {favorite_number['salad']}")
