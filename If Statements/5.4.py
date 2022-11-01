805db4b25ec0ae2fd5497b07d5848552790cbf22
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 12:27:16 +0800

    Added 5.4.py

---
+++ If Statements/5.4.py
@@ -0,0 +1,8 @@
+alien_color = "green"
+
+if alien_color is "green":
+    print("You've just earned 5 Points")
+elif alien_color is not "green":
+    print("You've just earned 10 points")
+else:
+    print("No points earned")
