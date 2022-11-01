774be45a6618491d304b7698d58e763e98d05179
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 11:31:29 +0800

    Added 4.13.py

---
+++ Working with Lists/4.13.py
@@ -0,0 +1,9 @@
+buffets = ('water', 'melon', 'papaya', 'banana')
+for buffet in buffets:
+    print(buffet)
+
+# buffets[0] = "watermelon" -> error
+
+buffets = ('water', 'melon', 'papaya', 'banana', 'fruit', 'salad')
+for buffet in buffets:
+    print(buffet)
