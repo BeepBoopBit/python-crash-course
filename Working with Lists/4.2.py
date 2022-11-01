e8cab03247140a8f65fc626c3a4c2886a57ddf6e
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 09:47:52 +0800

    Added 4.2.py

---
+++ Working with Lists/4.2.py
@@ -0,0 +1,7 @@
+animals = ['water','melon','papaya','banana']
+
+for animal in animals:
+    print(animal)
+for animal in animals:
+    print(f"{animal} would be a great pet")
+print("Any of this animal would be a great pet")
