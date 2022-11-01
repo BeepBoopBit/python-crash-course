233c634c2fc19d9927bbe308438526a11bbdd693
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 08:46:47 +0800

    Added 3.3.py

---
+++ Lists/3.3.py
@@ -0,0 +1,10 @@
+mode_of_transportation = ["water", "melon", "papaya", "banana"]
+stores = ["water store", "melon store", "papaya store", "banana store"]
+
+message = "I would like to use ";
+conjunction = " to go to the ";
+
+print(f"{message}{mode_of_transportation[0]}{conjunction}{stores[0]}")
+print(f"{message}{mode_of_transportation[1]}{conjunction}{stores[1]}")
+print(f"{message}{mode_of_transportation[2]}{conjunction}{stores[2]}")
+print(f"{message}{mode_of_transportation[3]}{conjunction}{stores[3]}")
