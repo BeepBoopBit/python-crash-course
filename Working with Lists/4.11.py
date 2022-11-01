19f543808aec85839cd73576f132094f70236707
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 10:26:17 +0800

    Added 4.11.py

---
+++ Working with Lists/4.11.py
@@ -0,0 +1,8 @@
+pizzas = ['water', 'melon', 'papaya', 'banana','fruit', 'salad']
+friend_pizzas = pizzas[:]
+pizzas.append("watermelon")
+friend_pizzas.append("fruit salad")
+
+print(pizzas)
+print(friend_pizzas)
+
