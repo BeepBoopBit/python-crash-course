f759c45f014a6324ecd424a46611d9d91f28879f
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-01 08:44:03 +0800

    Added 3.2.py

---
+++ Lists/3.2.py
@@ -0,0 +1,7 @@
+friends = ['water', 'melon', 'papaya', 'banana']
+greet = "Hello"
+message = ", I hope you have a good day"
+print(f"{greet} {friends[0]}{message}")
+print(f"{greet} {friends[1]}{message}")
+print(f"{greet} {friends[2]}{message}")
+print(f"{greet} {friends[3]}{message}")
