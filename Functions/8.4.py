cb79d7abed62175dee4aad53516187eac1adf857
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-02 07:55:30 +0800

    Added 8.4.py

---
+++ Functions/8.4.py
@@ -0,0 +1,7 @@
+def make_shirt(size="Large", text_message="I Love Python"):
+    print("The shirt size is " + size + " and the text message is " + text_message)
+    
+make_shirt()
+make_shirt("Large")
+make_shirt("Medium")
+make_shirt("Small", "I Love Watermelon")
