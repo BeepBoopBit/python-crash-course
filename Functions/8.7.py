d91287a980ff6473f35a795c9dfb5dbce666526a
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-02 08:05:49 +0800

    Change 8.7.py

--- Functions/8.7.py
+++ Functions/8.7.py
@@ -1,7 +1,9 @@
 from logging.config import _RootLoggerConfiguration
 
 
-def make_album(artist_name, album_title):
+def make_album(artist_name, album_title, number_of_tracks = None):
+    if number_of_tracks:
+        return {artist_name: {album_title: number_of_tracks}}
     return {artist_name: album_title}
 
 dict0 = make_album("The Beatles", "Abbey Road")
