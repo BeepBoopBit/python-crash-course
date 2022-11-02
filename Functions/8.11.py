8f15ddd550f72707f844255a5e791268673d13ef
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-11-02 08:25:03 +0800

    added 8.11.py

---
+++ Functions/8.11.py
@@ -0,0 +1,11 @@
+def show_message(message, newDir):
+    while message:
+        current = message.pop()
+        print(current)
+        newDir.append(current)
+        
+listA = ['water','melon','papaya', 'banana', 'fruit', 'salad']
+sentMessages = []
+
+show_message(listA[:], sentMessages)
+print(sentMessages)
